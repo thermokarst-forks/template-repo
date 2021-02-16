43c9258003976e7e157d23eb40bf1c4c74365523
Matthew Dillon <matthewrdillon@gmail.com>
2021-02-16 15:46:48 -0700

    new: github actions/workflows

--- github_templates/update_templates.sh
+++ github_templates/update_templates.sh
@@ -7,20 +7,24 @@ set -e -x
 # This script doesn't work with bare git repos. It also assumes the caller
 # has push access to the remote repo.
 
-if [ $# -eq 0 ] || [ $# -gt 2 ];
+if [ $# -eq 0 ] || [ $# -gt 4 ];
   then
     cat <<EOF
-Usage: $0 \$LOCAL_REPO \$REMOTE
+Usage: $0 \$LOCAL_REPO \$PACKAGE_NAME \$ADDITIONAL_TESTS \$REMOTE
 
-\$LOCAL_REPO      path to local repo
-\$REMOTE          alias for desired remote to push to (defined in \$LOCAL_REPO)
+\$LOCAL_REPO          path to local repo
+\$PACKAGE_NAME        name to assign to conda package
+\$ADDITIONAL_TESTS    additional tests to pass to ci.yml
+\$REMOTE              alias for desired remote to push to (defined in \$LOCAL_REPO)
 EOF
 exit 0
 fi
 
 # args & vars
 destination_repo=$1
-remote=$2
+package_name=$2
+additional_tests=$3
+remote=$4
 local_branch="master"
 templates_dir="templates"
 destination_dir="$destination_repo/.github"
@@ -30,8 +34,17 @@ commit_msg="MAINT: Updating GitHub templates"
 validate_repo "$destination_repo"
 prep_dest "$destination_repo" "$local_branch"
 cp -r ./$templates_dir/* $destination_dir
+if [ $package_name ]; then
+  workflows_dir="$destination_dir/workflows"
+  mkdir $workflows_dir
+  sed -e "s;%PACKAGE_NAME%;$package_name;g" \
+      -e "s;%ADDITIONAL_TESTS%;$additional_tests;g" \
+      ./ci.yml.tmpl \
+      > "$workflows_dir/ci.yml"
+fi
+
 commit_changes "$destination_dir" "$commit_msg"
 
 if [ $remote ]; then
-	push_changes "$destination_dir" "$remote" "$local_branch"
+  push_changes "$destination_dir" "$remote" "$local_branch"
 fi
